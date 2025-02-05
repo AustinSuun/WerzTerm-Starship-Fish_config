aeadd41fe85107b62a3d524d3180a2d39f94433a
Austin <96606879+AustinSuun@users.noreply.github.com>
2025-02-05 14:17:26 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,19 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/miniforge/bin/conda
-    eval /opt/miniforge/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/miniforge/etc/fish/conf.d/conda.fish"
-        . "/opt/miniforge/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/miniforge/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
-
 starship init fish | source
 
